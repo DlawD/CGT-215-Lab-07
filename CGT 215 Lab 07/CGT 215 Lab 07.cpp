d5246e09f5db0f6ea5c116638adbcf21cb1241ca
Yichen Dai <dai229@purdue.edu>
2022-10-18 23:26:19 -0400

    Add project files.

---
+++ CGT 215 Lab 07/CGT 215 Lab 07.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+
+using namespace std;
+using namespace sf;
+using namespace sfp;
+
+int main()
+{
+	// Create our window and world with gravity 0,1
+	RenderWindow window(VideoMode(800, 600), "Bounce");
+	World world(Vector2f(0, 1));
+
+	// Create the ball
+	PhysicsCircle ball;
+	ball.setCenter(Vector2f(300, 300));
+	ball.setRadius(20);
+	ball.applyImpulse(Vector2f(0.5, 0.5));
+	world.AddPhysicsBody(ball);
+
+	// Create the floor
+	PhysicsRectangle floor;
+	floor.setSize(Vector2f(800, 20));
+	floor.setCenter(Vector2f(400, 590));
+	floor.setStatic(true);
+	world.AddPhysicsBody(floor);
+
+	// Create the leftside
+	PhysicsRectangle leftside;
+	leftside.setSize(Vector2f(20, 560));
+	leftside.setCenter(Vector2f(10, 300));
+	leftside.setStatic(true);
+	world.AddPhysicsBody(leftside);
+
+	// Create the rightside
+	PhysicsRectangle rightside;
+	rightside.setSize(Vector2f(20, 560));
+	rightside.setCenter(Vector2f(790, 300));
+	rightside.setStatic(true);
+	world.AddPhysicsBody(rightside);
+
+	// Create the ceiling
+	PhysicsRectangle ceiling;
+	ceiling.setSize(Vector2f(800, 20));
+	ceiling.setCenter(Vector2f(400, 10));
+	ceiling.setStatic(true);
+	world.AddPhysicsBody(ceiling);
+
+	// Create the centerblock
+	PhysicsRectangle center;
+	center.setSize(Vector2f(100, 100));
+	center.setCenter(Vector2f(400, 300));
+	center.setStatic(true);
+	world.AddPhysicsBody(center);
+
+	int thudCount(0);
+	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+	};
+
+	int bangCount(0);
+	center.onCollision = [&bangCount](PhysicsBodyCollisionResult result) {
+		cout << "bang " << bangCount << endl;
+		bangCount++;
+	};
+
+	Clock clock;
+	Time lastTime(clock.getElapsedTime());
+
+	while (bangCount < 2) {
+		// calculate MS since last frame
+		Time currentTime(clock.getElapsedTime());
+		Time deltaTime(currentTime - lastTime);
+		int deltaTimeMS(deltaTime.asMilliseconds());
+		if (deltaTimeMS > 0) {
+			world.UpdatePhysics(deltaTimeMS);
+			lastTime = currentTime;
+		}
+		window.clear(Color(0, 0, 0));
+		window.draw(ball);
+		window.draw(floor);
+		window.draw(leftside);
+		window.draw(rightside);
+		window.draw(ceiling);
+		window.draw(center);
+		window.display();
+	}
+
+}
